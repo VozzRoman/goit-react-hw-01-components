fa55f8236bb4e5351f0309f948f1108a3e50ad63
EvaVozz <voznukeeva@ukr.net>
2022-12-13 12:30:43 +0200

    adding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Profile } from "./Profile/Profile";
+import dataProfile from "../user";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={dataProfile.username}
+        tag={dataProfile.tag}
+        location={dataProfile.location}
+        avatar={dataProfile.avatar}
+        stats={dataProfile.stats}  
+      />
     </div>
+    
+   
   );
 };
